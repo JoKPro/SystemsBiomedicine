d82325d642c645ffa91f01081ca04e670d18482b
JoKPro <johannp@esplus.de>
2021-11-13 12:48:41 +0100

    * loaded proportions from article * plot the proportions in the article * tried to reshape the quantiseq results s.t. it resembles the article data

--- hw3.R
+++ hw3.R
@@ -28,9 +28,8 @@ CountMatrix <- 2^CountMatrix
 # quantiseq
 res_quantiseq <- immunedeconv::deconvolute(CountMatrix, method = "quantiseq", arrays = T, tumor = F)
 res_quantiseq <- melt(as.data.table(res_quantiseq), id.vars = "cell_type", variable.name = "sample", value.name = "proportion")
-
+res_quantiseq$cell_type <- as.factor(res_quantiseq$cell_type)
 ggplot(res_quantiseq, aes(x = sample, y = proportion, fill = cell_type)) + geom_col(position = "fill")
-
 #--------------------------------------------------------
 # timer
 res_timer <- immunedeconv::deconvolute(CountMatrix, method = "timer", arrays = T, tumor = F)
@@ -60,3 +59,32 @@ ggplot(res_xcell, aes(x = sample, y = proportion, fill = cell_type)) + geom_col(
 res_epic <- immunedeconv::deconvolute(CountMatrix, method = "epic", arrays = T, tumor = F)
 res_epic <- melt(as.data.table(res_epic), id.vars = "cell_type", variable.name = "sample", value.name = "proportion")
 ggplot(res_epic, aes(x = sample, y = proportion, fill = cell_type)) + geom_col(position = "fill")
+
+
+###-------------------------------------------------------------------------------------------------------
+# load real proportions
+load("../../extdata/data/hw3/kidneyTransplant.RData")
+cellFreq <- data.frame(cellFreq)
+cellFreq["patientGroup"] <- as.factor(patientGroups)
+names(cellFreq) <- c(cellNames, "patientGroup")
+cellFreq <- melt(as.data.table(cellFreq), id.vars = "patientGroup", variable.name = "cell_type", value.name = "proportion")
+levels(cellFreq$patientGroup) <- c("normal", "AR")
+
+ggplot(cellFreq, aes(x = patientGroup, y = proportion, fill = cell_type)) + geom_col(position = "fill")
+ggplot(cellFreq, aes(x = cell_type, y = proportion, fill = patientGroup)) + geom_col(position = "fill") 
+
+
+## put all the data in one table
+## quantiseq: combine cell types to lymphocytes
+levels(res_quantiseq$cell_type)[res_quantiseq$cell_type %in% c("B cell", "NK cell", 
+                                                               "T cell regulatory (Tregs)",
+                                                               "T cell CD4+ (non-regulatory)",
+                                                               "T cell CD8+")] <- "Lymphocytes"
+levels(res_quantiseq$cell_type)[res_quantiseq$cell_type %in% c("Macrophage M1", "Macrophage M2", "Monocyte")] <- "Monocytes"
+sample_desc = c("normal", "AR", "AR", "AR", "AR", "AR", "AR", "normal", "normal", "AR", "normal", "AR", "AR", "AR", "AR",
+                "normal", "normal", "normal", "normal", "AR", "AR", "AR", "normal","AR")
+tmp <- data.frame(cbind(levels(res_quantiseq$sample),sample_desc, deparse.level = 1))
+names(tmp) <- c("sample", "group")
+res_quantiseq <- merge(res_quantiseq, tmp, by = "sample", all.y = T)
+
+ggplot(res_quantiseq, aes(group, proportion, fill = cell_type)) + geom_col(position = "fill") 
